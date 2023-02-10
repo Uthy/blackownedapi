4e86326299e54302afa82c60cd917658908408b2
Uthman Olomide <generaluthy@gmail.com>
2023-02-10 14:51:06 -0500

    added missing 'api' in base url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const isProduction = (process.env.NODE_ENV === 'production') ? true : false ;
-const DOMAIN_NAME = 'blackbizapi.com';
+const DOMAIN_NAME = 'api.blackbizapi.com';
 const PORT = 80;
 
 const SWAGGER_SETTINGS = {
